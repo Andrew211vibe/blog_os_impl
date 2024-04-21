b10a89b23215dbd94f16268a7a942cf54c792181
Andrew211vibe <1207368051@qq.com>
2024-04-21 17:35:06 +0800

    update macro

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 // This function will be called when `panic`
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
@@ -24,9 +35,10 @@ pub extern "C" fn _start() -> ! {
 //        }
 //    }
 //    vga_buffer::print_something();
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
