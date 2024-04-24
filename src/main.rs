0b09e32e1b7c29100f71d5ad1923b1996e0024f9
Andrew211vibe <1207368051@qq.com>
2024-04-24 16:00:13 +0800

    update cpu interrupt

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,15 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
